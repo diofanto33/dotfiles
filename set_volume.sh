983ce110d33e3e11fc35118758fd92a6f8db0dd6
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-05-03 22:20:34 -0300

    [ADD] volume script

---
+++ set_volume.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+function set_audio_level {
+	ar1=$1
+	if [[ $ar1 = "up" ]]; then
+		volume_level="$(amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }')"
+		volume_level="${volume_level%?}"
+		##keep a 150 limit to the audio level
+		if ((volume_level < 150)); then
+			pactl set-sink-volume 0 +5%
+		fi
+	elif [[ $ar1 = "down" ]]; then
+		pactl set-sink-volume 0 -5%
+	elif [[ $ar1 = "mute" ]]; then
+		pactl set-sink-mute 0 toggle
+	fi
+}
+
+set_audio_level $1
