23376e9986d044391e4c47d9151ea23bb1b848bc
diego rodriguez <diego.rodriguez.ramirez@mi.unc.edu.ar>
2023-05-30 09:24:08 -0300

    [UPD] Fixed tabs bug

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -1,4 +1,11 @@
--- vim.o.guifont = "MonoSpace Regular Bold h12"
+-- Establecer el número de lineas
 vim.api.nvim_command("set number")
-vim.api.nvim_command("set tabstop=4")
+
+-- Establecer el ancho de indentación a 4 espacios
 vim.api.nvim_command("set shiftwidth=4")
+
+-- Establecer el ancho de tabulación a 4 espacios
+vim.api.nvim_command("set tabstop=4")
+
+-- Convertir las tabulaciones en espacios (importante para github)
+vim.api.nvim_command("set expandtab")
